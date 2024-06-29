7a7a92b1aa81b3b5f731e86b7eff5a9d7f591a15
LEANDRO GOUVEA DA SILVA <leandro.gou@live.com>
2024-06-28 21:43:51 -0300

    [Leandro Gouvea] Adicionando novo Repositorio. Respondendo questão 1 e 2

---
+++ src/Questao1/ContaBancaria.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace Questao1
+{
+    class ContaBancaria
+    {
+        public long Conta { get; }
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+
+        public ContaBancaria(long conta, string titular, double saldo)
+        {
+            Conta = conta;
+            Titular = titular;
+            Saldo = saldo;
+        }
+        public ContaBancaria(long conta, string titular)
+        {
+            Conta = conta;
+            Titular = titular;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+        public void Saque(double valor)
+        {
+            Saldo -= (valor+3.50);
+        }
+        public override string ToString()
+        {
+            return string.Format("Conta {0}, Titular: {1}, Saldo: $ {2}",Conta,Titular,Saldo.ToString("n2",CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ src/Questao2/Program.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
+
+public class Program
+{
+    public static void Main()
+    {
+        string teamName = "Paris Saint-Germain";
+        int year = 2013;
+        int totalGoals = getTotalScoredGoals(teamName, year);
+
+        Console.WriteLine("Team "+ teamName +" scored "+ totalGoals.ToString() + " goals in "+ year);
+
+        teamName = "Chelsea";
+        year = 2014;
+        totalGoals = getTotalScoredGoals(teamName, year);
+
+        Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
+
+        // Output expected:
+        // Team Paris Saint - Germain scored 109 goals in 2013
+        // Team Chelsea scored 92 goals in 2014
+    }
+
+    private static int getTotalScoredGoals(string team, int year)
+    {
+        int totalGoals = 0;
+        totalGoals += ProcessarAPI("team1", team, year);
+        totalGoals += ProcessarAPI("team2", team, year);
+        return totalGoals;
+    }
+    private static int ProcessarAPI(string casaVisitante,string team,int year)
+    {
+        int pagina = 1;
+        int gols = 0;
+        JObject jo = new JObject();
+        do
+        {
+            string link = string.Format("https://jsonmock.hackerrank.com/api/football_matches?year={0}&{1}={2}&page={3}", year,casaVisitante, team, pagina);
+            jo = AcessarAPI(link);
+            foreach (var item in jo["data"])
+            {
+                gols += item.Value<int>(casaVisitante+"goals");
+            }
+            pagina++;
+        } while (pagina <= jo.Value<int>("total_pages"));
+        return gols;
+    }
+
+    private static JObject AcessarAPI(string link)
+    {
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri(link);
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+        JObject json = JObject.Parse(client.GetStringAsync(client.BaseAddress).Result);
+
+        return json;
+    }
+
+}
